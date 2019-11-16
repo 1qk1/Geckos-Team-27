455df188bcbece82dc9c20ac61299e0ff4c12655
1qk1 <1qk231@gmail.com>
2019-11-17 01:24:39 +0200

    fix axios on prod

--- client/src/index.js
+++ client/src/index.js
@@ -7,7 +7,10 @@ import { PersistGate } from "redux-persist/es/integration/react";
 import configureStore from "./redux/configureStore";
 import axios from "axios";
 
-axios.defaults.baseURL = `http://localhost:${PORT || 4000}/api`;
+axios.defaults.baseURL = `http://localhost:4000/api`;
+if (process.env.NODE_ENV === "production") {
+  axios.defaults.baseURL = `/api`;
+}
 
 const { store, persistor } = configureStore();
 
