a03dc04a7e27a1c8dcbd02c9dee850f930aa67d4
1qk1 <1qk231@gmail.com>
2019-11-17 12:07:45 +0200

    comment out axios baseurl

--- client/src/index.js
+++ client/src/index.js
@@ -7,11 +7,11 @@ import { PersistGate } from "redux-persist/es/integration/react";
 import configureStore from "./redux/configureStore";
 import axios from "axios";
 
-if (process.env.NODE_ENV === "production") {
-  axios.defaults.baseURL = `/api`;
-} else {
-  axios.defaults.baseURL = `http://localhost:4000/api`;
-}
+// if (process.env.NODE_ENV === "production") {
+//   axios.defaults.baseURL = `/api`;
+// } else {
+//   axios.defaults.baseURL = `http://localhost:4000/api`;
+// }
 
 const { store, persistor } = configureStore();
 
