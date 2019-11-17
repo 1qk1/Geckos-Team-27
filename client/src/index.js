7e51ac37bebd713e7fb90f40f5b23c8658db6887
1qk1 <1qk231@gmail.com>
2019-11-17 12:15:33 +0200

    remove axios baseurl

--- client/src/index.js
+++ client/src/index.js
@@ -5,13 +5,6 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/es/integration/react";
 import configureStore from "./redux/configureStore";
-import axios from "axios";
-
-// if (process.env.NODE_ENV === "production") {
-//   axios.defaults.baseURL = `/api`;
-// } else {
-//   axios.defaults.baseURL = `http://localhost:4000/api`;
-// }
 
 const { store, persistor } = configureStore();
 
