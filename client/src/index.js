756c9b29a029f17aca205b34f54eb04aa25b5d07
1qk1 <1qk231@gmail.com>
2019-11-17 11:24:51 +0200

    trying to make heroku work

--- client/src/index.js
+++ client/src/index.js
@@ -7,9 +7,10 @@ import { PersistGate } from "redux-persist/es/integration/react";
 import configureStore from "./redux/configureStore";
 import axios from "axios";
 
-axios.defaults.baseURL = `http://localhost:4000/api`;
 if (process.env.NODE_ENV === "production") {
   axios.defaults.baseURL = `/api`;
+} else {
+  axios.defaults.baseURL = `http://localhost:4000/api`;
 }
 
 const { store, persistor } = configureStore();
